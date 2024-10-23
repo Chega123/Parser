8b49502fe3836af41eb49a976c3e93f6baabca65
Chega123 <85765351+Chega123@users.noreply.github.com>
2024-10-22 23:04:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include "parser.hpp"
+#include <vector>
 #include <iostream>
-int main() {
 
-  /*
-  Parser parser;
-  if (parser.parse()) {
-    std::cout << ":)" << std::endl;
-  } else {
-    std::cout << ":(" << std::endl;
-  }
-  */
-  return 0;
+
+int main() {
+    // Definir la secuencia de tokens: IntType [ ] Identifier = CharLiteral ;
+    std::vector<Token> tokens = {
+        Token("IntType"),
+        Token("Token_["),
+        Token("Token_]"),
+        Token("Token_ID"),
+        Token("Token_="),
+        Token("Token_Comilla"),
+        Token("Token_ID"),
+        Token("Token_Comilla"),
+        Token("Token_;")
+    };
+
+    // Crear el objeto Parser con el vector de tokens
+    Parser parser(tokens);
+
+    // Ejecutar el análisis sintáctico
+    if (parser.parse()) {
+        std::cout << "Parsing exitoso." << std::endl;
+    }
+    else {
+        std::cout << "Error de parsing." << std::endl;
+    }
+
+    return 0;
 }
